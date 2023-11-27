7a75d2a0d93ebaa6a4c73b3827c9d02fe6bd5970
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2023-11-27 16:17:47 +0100

    DSA comments

--- docs/examples/DSA.js
+++ docs/examples/DSA.js
@@ -14,15 +14,16 @@ export function apply_DSA_Mask(
   const metadata_info=multiFrameSerie[tag];
   const mask_type=metadata_info["0"].x00286101;
   const frame_index_number=metadata_info["0"].x00286110;
-  const frameId=multiFrameSerie.instances["multiFrameLoader://0?frame="+frame_index_number]
+  const frameId=multiFrameSerie.instances["multiFrameLoader://0?frame="+(frame_index_number-1)]
   if(mask_type==="NONE")
   {
     return;
   }
   else if (mask_type==="AVG_SUB")
   {
-     /*(Average Subtraction) The frames specified by the Mask Frame Numbers x00286110 
-     are averaged together, shifted by the amount specified in the Mask Sub-pixel Shift x00286114, 
+     /*(Average Subtraction) 
+     The frames specified by the Mask Frame Numbers x00286110 are averaged together, 
+     shifted by the amount specified in the Mask Sub-pixel Shift x00286114, 
      then subtracted from the contrast frames in the range specified in the Applicable 
      Frame Range x00286102. 
      Contrast Frame Averaging x00286112 number of frames starting with the current 
