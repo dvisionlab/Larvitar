d80dcc9558a9be79cc7a30845d48f99d23873b2f
alessia-z <alessia.zanni@dvisionlab.com>
2025-03-21 11:54:03 +0100

    test 4d

--- cypress/e2e/4d.cy.js
+++ cypress/e2e/4d.cy.js
@@ -32,6 +32,65 @@ describe("Testing the 4d.html functionalities", () => {
     // Wait for files to be loaded
     cy.window().its("allFilesLoaded").should("eq", true, { timeout: 10000 });
     cy.get("#spinner").should("not.be.visible");
-    const imageIndex = larvitar.store.get(["viewports", "viewer", "sliceId"]);
+  });
+
+  it("should handle slices mode", () => {
+    cy.window()
+      .its("larvitar")
+      .then(larvitar => {
+        const imageIndex = larvitar.store.get([
+          "viewports",
+          "viewer",
+          "sliceId"
+        ]);
+        cy.wrap(imageIndex).as("initialimageIndex");
+      });
+    cy.get("#viewer").trigger("wheel");
+    cy.window()
+      .its("larvitar")
+      .then(larvitar => {
+        const imageIndex = larvitar.store.get([
+          "viewports",
+          "viewer",
+          "sliceId"
+        ]);
+        cy.get("@initialimageIndex").then(initialimageIndex => {
+          expect(imageIndex).to.not.equal(initialimageIndex);
+        });
+      });
+    cy.get("#slicenum").should("exist")
+    cy.get("#slicenum").invoke("text").then(text => { expect(text).to.contain("Slice Number: 2 of 2") })
+  });
+
+  it("should toggle frames mode", () => {
+    cy.get("#toggleButton").click()
+    cy.get("#animation").should("exist")
+    cy.get("#animation").invoke("text").then(text => { expect(text).to.contain("Scroll Mode Active: Frames.") })
+
+    cy.window()
+      .its("larvitar")
+      .then(larvitar => {
+        const imageIndex = larvitar.store.get([
+          "viewports",
+          "viewer",
+          "sliceId"
+        ]);
+        cy.wrap(imageIndex).as("initialimageIndex");
+      });
+    cy.get("#viewer").trigger("wheel");
+    cy.window()
+      .its("larvitar")
+      .then(larvitar => {
+        const imageIndex = larvitar.store.get([
+          "viewports",
+          "viewer",
+          "sliceId"
+        ]);
+        cy.get("@initialimageIndex").then(initialimageIndex => {
+          expect(imageIndex).to.not.equal(initialimageIndex);
+        });
+      });
+    cy.get("#image-time").should("exist")
+    cy.get("#image-time").invoke("text").then(text => { expect(text).to.contain("Image Time Id: 1 of 96") })
   });
 });
