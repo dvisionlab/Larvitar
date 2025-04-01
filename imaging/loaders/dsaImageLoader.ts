004a82d146783b7f749be96368f097c8d7eee923
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2025-04-01 08:54:37 +0200

    wwwl from srcImage

--- imaging/loaders/dsaImageLoader.ts
+++ imaging/loaders/dsaImageLoader.ts
@@ -140,16 +140,14 @@ let createCustomImage = function (
   let promise: Promise<Image> = new Promise((resolve, _) => {
     const minPixelValue = getMinPixelValue(pixelData);
     const maxPixelValue = getMaxPixelValue(pixelData);
-    const windowWidth = (maxPixelValue - minPixelValue) / 2;
-    const windowCenter = windowWidth / 2;
     const image: Partial<Image> = {
       imageId: imageId,
       minPixelValue: minPixelValue,
       maxPixelValue: maxPixelValue,
       slope: srcImage.slope,
       intercept: srcImage.intercept,
-      windowCenter: windowCenter,
-      windowWidth: windowWidth,
+      windowCenter: srcImage.windowCenter,
+      windowWidth: srcImage.windowWidth,
       getPixelData: () => pixelData,
       rows: srcImage.rows,
       columns: srcImage.columns,
