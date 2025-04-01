5b54e8ee65a726de7632189711f24acd616ae887
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2025-04-01 14:47:27 +0200

    fixed formulas

--- imaging/loaders/dsaImageLoader.ts
+++ imaging/loaders/dsaImageLoader.ts
@@ -140,14 +140,15 @@ let createCustomImage = function (
   let promise: Promise<Image> = new Promise((resolve, _) => {
     const minPixelValue = getMinPixelValue(pixelData);
     const maxPixelValue = getMaxPixelValue(pixelData);
-    const computedWindowWidth = (maxPixelValue - minPixelValue) / 2;
+    const computedWindowWidth = maxPixelValue - minPixelValue;
+    const computedWindowCenter = (maxPixelValue + minPixelValue) / 2;
     const image: Partial<Image> = {
       imageId: imageId,
       minPixelValue: minPixelValue,
       maxPixelValue: maxPixelValue,
       slope: srcImage.slope,
       intercept: srcImage.intercept,
-      windowCenter: srcImage.windowCenter || computedWindowWidth / 2,
+      windowCenter: srcImage.windowCenter || computedWindowCenter,
       windowWidth: srcImage.windowWidth || computedWindowWidth,
       getPixelData: () => pixelData,
       rows: srcImage.rows,
