ecce489bbbaaa4e72d8f42e54c8ba056b8dd64d2
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2025-04-04 15:28:46 +0200

    correct repo paths

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,14 +3,11 @@ const {
   addMatchImageSnapshotPlugin
 } = require("cypress-image-snapshot/plugin");
 
-module.exports = defineConfig({
+export default defineConfig({
   defaultCommandTimeout: 20000,
   reporter: "cypress-multi-reporters",
   reporterOptions: {
-    reporterEnabled: "spec, mocha-json-reporter",
-    mochaJsonReporterReporterOptions: {
-      output: "cypress-test/test-result.json"
-    }
+    reporterEnabled: "spec, mocha-json-reporter"
   },
   e2e: {
     setupNodeEvents(on, config) {
