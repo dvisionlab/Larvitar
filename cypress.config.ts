07e33ef7fd423e32c422adf65b3414f5663d653d
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2025-04-04 14:34:05 +0200

    fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,13 @@ const {
 } = require("cypress-image-snapshot/plugin");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 20000,
+  reporter: "cypress-multi-reporter",
+  reporterOptions: {
+    reporterEnabled: "spec, json",
+    jsonReporterOptions: {
+      output: "cypress-test/test-result.json"
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
