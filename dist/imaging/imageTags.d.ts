73736c23801006981068b0bc774c9991f7d0a396
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2025-05-12 10:57:11 +0200

    push build to test

---
+++ dist/imaging/imageTags.d.ts
@@ -0,0 +1,32 @@
+import { DataSet, Element } from "dicom-parser";
+import type { MetaDataTypes } from "./MetaDataTypes";
+/**
+ * Parse a DICOM Tag according to its type
+ * @instance
+ * @function parseTag
+ * @param {Object} dataSet - The parsed dataset object from dicom parser
+ * @param {String} propertyName - The tag name
+ * @param {Object} element - The parsed dataset element
+ * @return {String} - The DICOM Tag value
+ */
+export declare function parseTag<T>(dataSet: DataSet, propertyName: string, //x0000000 string
+element: Element): Element | T | undefined;
+/**
+ * Gets nested objects inside item
+ * @instance
+ * @function getNestedObject
+ * @param {Element} item - The nested starting item
+ * @param {MetaDataTypes[]} nestedArray - Nested array that will contain the nested object
+ * @return {string} - The fixed charset
+ */
+export declare function getNestedObject(item: Element, nestedArray: MetaDataTypes[]): void;
+/**
+ * Extract tag value according to its value rapresentation, see
+ * {@link http://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html}
+ * @instance
+ * @function getTagValue
+ * @param {Object} dataSet - the dataset
+ * @param {String} tag - the desired tag key
+ * @return {Number | Array | String} - the desired tag value
+ */
+export declare const getTagValue: (dataSet: DataSet, tag: string) => any;
---
+++ dist/imaging/tools/custom/rectangleRoiOverlayTool.d.ts
@@ -0,0 +1,45 @@
+/**
+ * @public
+ * @class RectangleRoiTool
+ * @memberof Tools.Annotation
+ * @classdesc Tool for drawing rectangular regions of interest, and measuring
+ * the statistics of the enclosed pixels.
+ * @extends Tools.Base.BaseAnnotationTool
+ */
+export default class RectangleRoiOverlayTool {
+    constructor(props?: {});
+    throttledUpdateCachedStats: any;
+    createNewMeasurement(eventData: any): {
+        computeMeasurements: any;
+        visible: boolean;
+        active: boolean;
+        color: undefined;
+        invalidated: boolean;
+        handles: {
+            start: {
+                x: any;
+                y: any;
+                highlight: boolean;
+                active: boolean;
+            };
+            end: {
+                x: any;
+                y: any;
+                highlight: boolean;
+                active: boolean;
+            };
+            initialRotation: any;
+            textBox: {
+                active: boolean;
+                hasMoved: boolean;
+                movesIndependently: boolean;
+                drawnIndependently: boolean;
+                allowedOutsideImage: boolean;
+                hasBoundingBox: boolean;
+            };
+        };
+    } | undefined;
+    pointNearTool(element: any, data: any, coords: any, interactionType: any): boolean;
+    updateCachedStats(image: any, element: any, data: any): void;
+    renderToolData(evt: any): void;
+}
