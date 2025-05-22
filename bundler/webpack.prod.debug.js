55a228cdf2fe6f81a7de156e971a7f232af27bce
daron1337 <simone.manini@gmail.com>
2025-05-22 11:56:12 +0200

    4.0.0-alpha.1 w/ debug

---
+++ bundler/webpack.prod.debug.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const commonConfiguration = require("./webpack.common.js");
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = merge(commonConfiguration, {
+  devtool: false,
+  entry: path.resolve(__dirname, "../index.ts"),
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "larvitar.js",
+    library: "larvitar",
+    clean: true
+  },
+  mode: "production",
+  plugins: [new CleanWebpackPlugin(), new LodashModuleReplacementPlugin()],
+  optimization: {
+    minimize: false, // Disable minification for easier debugging
+    usedExports: true // Enable tree shaking,
+  }
+});
