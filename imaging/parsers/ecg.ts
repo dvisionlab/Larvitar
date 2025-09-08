8e42ab5e5a04380961cb69af8e4140789946f451
Laura Borghesi <146220917+laura-borghesi-lum00n@users.noreply.github.com>
2025-09-08 22:18:37 +0200

    fix: ecg single frame (#504)
    * fix ecg single frame
    
    * version
    
    * fix ecg conversion from base64
    
    * fixes

--- imaging/parsers/ecg.ts
+++ imaging/parsers/ecg.ts
@@ -3,8 +3,8 @@
  *         handling ECG signales in DICOM files
  */
 
-import { DataSet, Element } from "dicom-parser";
-import { getDataFromImageManager } from "../imageManagers";
+import { getDataFromImageManager, getImageManager } from "../imageManagers";
+import { MetaData } from "../types";
 
 /*
  * This module provides the following functions to be exported:
@@ -13,24 +13,37 @@ import { getDataFromImageManager } from "../imageManagers";
 
 /**
  * Generate an array of points representing the ECG signal
- * @instance
  * @function parseECG
- * @param {DataSet} dataSet - the DICOM dataset
- * @param {String} tag - the tag of the ECG signal
+ * @param {DataSet} seriesId - the series ID to which the ECG data belongs
+ * @param {MetaData} metadata - the metadata object containing the ECG signal data
  * @param {Number} nSampling - the sampling rate
  * @returns {void}
  */
-export function parseECG(
+export const parseECG = function (
   seriesId: string,
-  dataSet: DataSet,
-  tag: string,
+  metadata: MetaData,
   nSampling: number = 2
 ): void {
-  const element: Element = dataSet.elements[tag];
-  let data: Uint8Array = dataSet.byteArray.slice(
-    element.dataOffset,
-    element.dataOffset + element.length
-  );
+  const rawData = metadata["x50003000"];
+
+  let data: Uint8Array;
+  //case Qido retrieved Metadata where metadata["x50003000"] is a base64 string
+  if (typeof rawData === "string") {
+    try {
+      data = base64ToUint8Array(rawData);
+    } catch (e) {
+      console.error("Invalid Base64 ECG data", e);
+      return;
+    }
+  }
+  //case parsed Metadata where metadata["x50003000"] is a byteArray retrieved from the DataSet
+  else if (rawData instanceof Uint8Array) {
+    data = rawData;
+  } else {
+    console.warn("ECG data is missing or in an unsupported format.");
+    return;
+  }
+
   let points: number[] = [];
   const nCountFrom: number = data.length / 2;
   const nCountTo: number = Math.floor(0.5 + nCountFrom / nSampling);
@@ -51,4 +64,18 @@ export function parseECG(
   }
   let series = getDataFromImageManager(seriesId);
   series!.ecgData = points;
-}
+};
+
+/**
+ * @function base64ToUint8Array - Converts a Base64 encoded string to a Uint8Array.
+ * @param {string} base64 - The Base64 encoded string of the ECG signal data.
+ * @returns {Uint8Array} - The function assigns the result to a series object managed elsewhere.
+ */
+const base64ToUint8Array = function (base64: string): Uint8Array {
+  const binary = atob(base64);
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return bytes;
+};
