1ebe424498bc41643a9e5b88cbcf31a5b60d5f30
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2025-09-24 09:14:10 +0200

    fix filters on image edges

--- imaging/types.ts
+++ imaging/types.ts
@@ -373,9 +373,9 @@ type contrast = { windowCenter: number; windowWidth: number };
 type translation = { x: number; y: number };
 
 export type KernelConfig = {
-  label: string;
-  size: number;
-  kernel: number[][];
+  Label: string;
+  Size: number;
+  Kernel: number[][];
 };
 export type FilterImageFrame = {
   width: number;
