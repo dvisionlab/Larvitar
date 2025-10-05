6475d4c3b6e3580d235a8cd24f245aae3edf1291
Laura Borghesi <146220917+laura-borghesi-lum00n@users.noreply.github.com>
2025-10-05 15:52:31 +0200

    fix: frame number in updateLoadedStack (#514)
    * fix frame number in updateLoadedStack
    
    * readable check on frame
    
    * last fix on sliceIndex

--- imaging/imageLoading.ts
+++ imaging/imageLoading.ts
@@ -279,15 +279,16 @@ export const updateLoadedStack = function (
   else if (isMultiframe) {
     const imageId = seriesData.imageId as string;
     imageTracker[imageId] = lid as string;
-    if (sliceIndex !== undefined) {
+    if (sliceIndex !== undefined && sliceIndex !== null) {
       allSeriesStack[id].imageIds[sliceIndex] = imageId;
     } else {
       allSeriesStack[id].imageIds.push(imageId);
+      sliceIndex = allSeriesStack[id].imageIds.length - 1;
     }
 
     // store needed instance tags
     allSeriesStack[id].instances[imageId] = {
-      frame: sliceIndex ? sliceIndex : allSeriesStack[id].imageIds.length - 1,
+      frame: sliceIndex,
       instanceId: iid,
       metadata: seriesData.metadata
     };
